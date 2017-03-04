2cd0d44f0b12902fd495c93bc58a148fa499c2b4
Paul Aly <paul.aly@outlook.com>
2017-03-04 00:01:47 -0500

    stubbing out player action controller and spec

---
+++ src/js/player/PlayerActionControllerSpec.js
@@ -0,0 +1,9 @@
+describe('PlayerActionController', function () {
+    var playerActionController = require('./PlayerActionController');
+
+    describe('constructor', function () {
+        it('should not be undefined', function() {
+            expect(playerActionController).toBeTruthy();
+        });
+    });
+});
