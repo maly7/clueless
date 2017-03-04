aff1de936a5a6e3ac138066488e4f4b28314726c
Paul Aly <paul.aly@outlook.com>
2017-03-04 00:05:53 -0500

    stubbing out rules engine

---
+++ src/js/rules/RulesEngineSpec.js
@@ -0,0 +1,9 @@
+describe('RulesEngine', function () {
+    var rulesEngine = require('./RulesEngine');
+
+    describe('constructor', function () {
+        it('should not be undefined', function () {
+            expect(rulesEngine).toBeTruthy();
+        });
+    });
+});
