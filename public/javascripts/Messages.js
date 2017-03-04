7c6ebc832f606bc9f2c42c02c8c06e4eb624587e
Paul Aly <paul.aly@outlook.com>
2017-03-04 16:16:53 -0500

    more socket stuff

--- public/javascripts/Messages.js
+++ public/javascripts/Messages.js
@@ -2,10 +2,35 @@ var io = require('socket.io-client');
 var $ = require('jquery');
 var socket = io.connect('http://localhost:3000');
 
-socket.on('player-joined', function (data) {
-    addMessage('Welcome Player ' + data.id);
+$('#send-btn').click(function() {
+    var text = getChatInput();
+    sendButtonClicked(text);
+    clearChatInput();
+    return false;
 });
 
-var addMessage = function(message) {
+socket.on('player-joined', function (data) {
+    addMessage(data.message);
+});
+
+socket.on('chat message', function(data) {
+    addMessage(data.message);
+});
+
+var addMessage = function (message) {
     return $('#messages').append($('<li>').text(message));
 };
+
+var sendButtonClicked = function (text) {
+    socket.emit('chat message', {
+        message: text
+    });
+};
+
+var getChatInput = function() {
+    return $('#chat-box').val();
+};
+
+var clearChatInput = function() {
+    return $('#chat-box').val('');
+}
