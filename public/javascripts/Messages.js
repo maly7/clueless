a8816637b8366e4656e65db50f5f960e173defa8
Paul Aly <paul.aly@outlook.com>
2017-03-12 11:59:04 -0400

    chat sockets work now

--- public/javascripts/Messages.js
+++ public/javascripts/Messages.js
@@ -2,35 +2,39 @@ var io = require('socket.io-client');
 var $ = require('jquery');
 var socket = io.connect('http://localhost:3000');
 
-$('#send-btn').click(function() {
-    var text = getChatInput();
-    sendButtonClicked(text);
-    clearChatInput();
-    return false;
-});
+window.onload = function () {
+    $('#send-btn').click(function () {
+        console.log('send clicked');
+        var text = getChatInput();
+        sendButtonClicked(text);
+        clearChatInput();
+        return false;
+    });
 
-socket.on('player-joined', function (data) {
-    addMessage(data.message);
-});
+    socket.on('player-joined', function (data) {
+        addMessage(data.message);
+    });
 
-socket.on('chat message', function(data) {
-    addMessage(data.message);
-});
+    socket.on('chat-message', function (data) {
+        addMessage(data.message);
+    });
+};
 
 var addMessage = function (message) {
     return $('#messages').append($('<li>').text(message));
 };
 
 var sendButtonClicked = function (text) {
-    socket.emit('chat message', {
+    console.log("Send button clicked with text: " + text);
+    socket.emit('chat-send', {
         message: text
     });
 };
 
-var getChatInput = function() {
+var getChatInput = function () {
     return $('#chat-box').val();
 };
 
-var clearChatInput = function() {
+var clearChatInput = function () {
     return $('#chat-box').val('');
 }
