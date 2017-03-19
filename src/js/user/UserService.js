551f9b16bfaa663b67ee111799ecb0742ad20721
Paul Aly <paul.aly@outlook.com>
2017-03-19 09:23:13 -0400

    extract out strip id function

--- src/js/user/UserService.js
+++ src/js/user/UserService.js
@@ -13,11 +13,14 @@
         };
     };
 
+    var stripId = function (id) {
+        return id.substring(id.indexOf('#') + 1);
+    };
+
     var addUser = function (id) {
         userCount++;
-        var plainId = id.substring(id.indexOf('#') + 1);
         var newUser = {
-            'id': plainId,
+            'id': stripId(id),
             'playerNumber': userCount
         };
         users.push(newUser);
@@ -29,8 +32,8 @@
         var playerNsp = io.of(PLAYER_NAMESPACE);
 
         playerNsp.on('connection', function (socket) {
-            var newPlayer = addUser(socket.id)
-            console.log(newPlayer.id);
+            var newPlayer = addUser(socket.id);
+
             playerNsp.emit('player-joined', {
                 'message': 'Hello Player ' + newPlayer.playerNumber,
                 'id': newPlayer.id
