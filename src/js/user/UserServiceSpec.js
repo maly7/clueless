859434fcad5d27f611271a1092beeb5ca8db3116
Paul Aly <paul.aly@outlook.com>
2017-03-19 09:35:37 -0400

    updating user service tests

--- src/js/user/UserServiceSpec.js
+++ src/js/user/UserServiceSpec.js
@@ -1,12 +1,27 @@
 describe('UserService', function () {
     var userService = require('./UserService');
 
+    beforeAll(function () {
+        userService.addUser('/players#abcdefg')
+    });
+
     describe('getUser', function () {
         it('should return a new user with the specified id', function () {
-            var id = 4;
+            var id = 'abcdefg';
             var user = userService.getUser(id);
 
             expect(user.id).toEqual(id);
+        });
+    });
+
+    describe('getCount', function () {
+        it('should return the current count of users', function () {
+            expect(userService.getCount()).toEqual(1);
+        });
+
+        it('should increase after a user has been added', function() {
+            userService.addUser('/players#hijkl');
+            expect(userService.getCount()).toEqual(2);
         });
     });
 });
