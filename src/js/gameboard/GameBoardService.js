6ff1732f8c6530078837ed5209cc79337b71ec62
Paul Aly <paul.aly@outlook.com>
2017-03-22 15:46:56 -0400

    extracting out function for notifying player of their turn

--- src/js/gameboard/GameBoardService.js
+++ src/js/gameboard/GameBoardService.js
@@ -3,6 +3,7 @@
     var _ = require('lodash');
 
     var GAME_NAMESPACE = '/game';
+    var gameNsp = {};
     var playerList = [];
     var gameRunning = false;
     var currentPlayer = 0;
@@ -17,16 +18,15 @@
         return player;
     };
 
-    var startGame = function (players) {
-        playerList.push(players);
-
-        // get current player
-        // notify current player of turn
-        // wait for end turn signal from current player
+    var notifyPlayerTurn = function () {
+        var player = getCurrentPlayer();
+        gameNsp.clients().sockets[GAME_NAMESPACE + '#' + player.id].emit('player-turn', {
+            'message': 'Player ' + player.playerNumber + '\'s turn'
+        });
     };
 
     var init = function (io, userService) {
-        var gameNsp = io.of(GAME_NAMESPACE);
+        gameNsp = io.of(GAME_NAMESPACE);
 
         gameNsp.on('connection', function (socket) {
             socket.on('start-game', function (data) {
@@ -40,16 +40,10 @@
 
                 playerList = userService.getPlayers();
                 gameRunning = true;
-                var player = getCurrentPlayer();
-                gameNsp.clients().sockets[GAME_NAMESPACE + '#' + player.id].emit('player-turn', {
-                    'message': 'Player ' + player.playerNumber + '\'s turn'
-                });
+                notifyPlayerTurn();
             });
             socket.on('end-turn', function (data) {
-                var player = getCurrentPlayer();
-                gameNsp.clients().sockets[GAME_NAMESPACE + '#' + player.id].emit('player-turn', {
-                    'message': 'Player ' + player.playerNumber + '\'s turn'
-                });
+                notifyPlayerTurn();
             });
         });
     };
