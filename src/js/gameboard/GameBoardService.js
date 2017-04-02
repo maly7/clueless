cd12abfa5135f485f3f16cd4715998bfd57a1c3d
Paul Aly <paul.aly@outlook.com>
2017-04-02 18:36:27 -0400

    working on backend for make accusation
    there's still a few kinks to work out

--- src/js/gameboard/GameBoardService.js
+++ src/js/gameboard/GameBoardService.js
@@ -14,12 +14,24 @@
 
     var getNextPlayer = function () {
         currentPlayer = playerList[currentPlayerIndex];
+        findNextEligiblePlayer();
+        return currentPlayer;
+    };
+
+    var findNextEligiblePlayer = function () {
         if (currentPlayerIndex + 1 >= playerList.length) {
             currentPlayerIndex = 0;
         } else {
             currentPlayerIndex++;
         }
-        return currentPlayer;
+
+        while (!playerList[currentPlayerIndex].active) {
+            if (currentPlayerIndex >= playerList.length) {
+                currentPlayerIndex = 0;
+            } else {
+                currentPlayerIndex++;
+            }
+        }
     };
 
     var notifyPlayerTurn = function () {
@@ -38,7 +50,7 @@
         });
     };
 
-    var notifyPlayerOfCards = function(player) {
+    var notifyPlayerOfCards = function (player) {
         gameNsp.clients().sockets[GAME_NAMESPACE + '#' + player.id].emit('cards', {
             'cards': player.cards,
             'extraCards': extraCards
@@ -50,6 +62,7 @@
         playerList = players;
         solution = cardDealer.selectMurderCase();
         extraCards = cardDealer.dealCardsToPlayers(playerList);
+        console.log('Game solution: ' + solution.suspect + ' with the ' + solution.weapon + ' in the ' + solution.room);
 
         var message = 'Player ' + playerNumber + ' started the game!';
         gameNsp.emit('game-started', {
@@ -60,6 +73,27 @@
 
         gameRunning = true;
         notifyPlayerTurn();
+    };
+
+    var checkAccusation = function (accusation) {
+        return accusation.weapon === solution.weapon &&
+            accusation.suspect === solution.suspect &&
+            accusation.room === solution.room;
+    };
+
+    var notifyWinningPlayer = function (id) {
+        return gameNsp.clients().sockets[id].emit('game-won', solution);
+    };
+
+    var notifyPlayersGameOver = function (socket) {
+        return socket.broadcast.emit('game-over', {
+            'solution': solution,
+            'player': currentPlayer.playerNumber
+        });
+    };
+
+    var notifyPlayerOfIncorrectAccusation = function (id) {
+        return gameNsp.clients().sockets[id].emit('game-lost', solution);
     };
 
     var init = function (io, userService, cardService) {
@@ -75,9 +109,17 @@
                 currentPlayer.position = data.position;
                 notifyPlayerTurn();
             });
-            socket.on('make-accusation', function(data) {
+            socket.on('make-accusation', function (data) {
                 var id = socket.id;
                 console.log('Player with id ' + id + ' accuses ' + data.suspect + ' with the ' + data.weapon + ' in the ' + data.room);
+                var isCorrectSolution = checkAccusation(data);
+                if (isCorrectSolution) {
+                    notifyWinningPlayer(id);
+                    notifyPlayersGameOver(socket);
+                } else {
+                    notifyPlayerOfIncorrectAccusation(id);
+                    currentPlayer.active = false;
+                }
             });
         });
     };
